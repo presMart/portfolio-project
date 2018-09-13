83389b2b94f34473e9d1e4e4aa6efaf9e77e203f
Preston Martin <presmart@hotmail.com>
2018-09-13 17:14:29 -0400

    Blog functionality and polish

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,20 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+# To list entries my name in admin panel
+    def __str__(self):
+        return self.title
+
+
+
+
+
 # Create blog model
 # title
 # pub_date
